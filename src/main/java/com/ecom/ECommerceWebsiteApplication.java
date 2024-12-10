bf2c0586a9784181f937d5000026b35656a8e8c9
sayalikakade10 <sayalikakade10@gmail.com>
2024-12-10 14:40:24 +0530

    Database Connection

--- src/main/java/com/ecom/ECommerceWebsiteApplication.java
+++ src/main/java/com/ecom/ECommerceWebsiteApplication.java
@@ -3,11 +3,16 @@ package com.ecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class ECommerceWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceWebsiteApplication.class, args);
+		
+		log.info("WELCOME TO SAYALI'S WEBSITE");
 	}
 
 }
